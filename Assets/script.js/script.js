9716493e0def07ac4cbb3ffbd97da729d385e73c
“Matthew <mpick90sick@gmail.com>
2023-08-11 16:29:36 -0400

    tried to adjust some code to no avail

--- Assets/script.js/script.js
+++ Assets/script.js/script.js
@@ -11,23 +11,22 @@ function writePassword() {
 }
 function generatePassword() {
   // code goes here
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12
   var password = ""
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber +1)
   }
-  document.getElimentById("#password").value = password;
+  document.getElementById("password").value = password;
   }
 
-function copyPassword() {
-  var copyText = document.getElementById("#password");
-  copyText.Select();
-  copyText.setSelectionRange(0, 999)
-  document.commandId("copy")
-}
+
+
 // Add event listener to generate button
-element.addEventListener("click", function() {
-  document.getElementById("#password").innerHTML = "#generate"
-});
+
+generateBtn.addEventListener("click", writePassword, function() {
+  generatePassword()
+})
+
+
